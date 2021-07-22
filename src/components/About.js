63d84136482f5ab4bd9a67b7e69e1c0f181aaded
William Walsh <wmdwalsh0@gmail.com>
2021-07-21 23:06:40 -0400

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2 style={{color:'black'}}>About Me</h2>
+      <p>I made this</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+import { name, city } from "../data/data"
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{color:'firebrick'}}>
+        {name} is a Web Developer from <br />{city}
+      </h1>
+    </div>
+  );
 }
 
-export default Home;
+export default Home; 
